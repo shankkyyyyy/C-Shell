20d49e90c530de056ffcb5372a86c90241143dae
shankkkyyyyy <ramanshikillu@gmail.com>
2026-04-16 22:37:12 +0530

    set

--- src/history.c
+++ src/history.c
@@ -1,44 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../include/history.h"
+//#include "../include/history.h"
 
 // define's
 #define BufferSize 1024
 
 // functions 
-char *History(FILE *fp,FILE *fp2,int times)
+char *History(FILE *fp,int times)
 {
-
-	char TemBuffer[BufferSize];
-	int CountOfLines = 0;
-	
-	while(fgets(TemBuffer,BufferSize,fp2)!=NULL)
+	int i = 1;
+	char tempbuffer[1024];
+	while (times > i && fgets(tempbuffer,BufferSize,fp)!=NULL)
 	{
-		CountOfLines++;
+		i++;
 		continue;
 	}
-	fclose(fp2);
-
-	int NumberOfLines = CountOfLines - times;
-	
-	// count
-	int i = 0;
-	
-	memset(TemBuffer,0,BufferSize);
-	
-	while(i!=NumberOfLines)
-	{
-		fgets(TemBuffer,BufferSize,fp);
-		//count
-		i++;
-	}
-	// last read.
-	
 	char *buffer = malloc(BufferSize);
-	fgets(buffer,BufferSize,fp);
-	//closing the file pointer;
-	fclose(fp);
+	if(fgets(buffer,BufferSize,fp)== NULL) return NULL;
 	return buffer;
 }
 
@@ -50,6 +29,7 @@ int AddHistory(char *command,char *filepath)
 		perror("error opening the temp file. \n");
 		return -1;
 	}
+	
 	fprintf(fp,"%s\n",command);
 	
 	FILE *fp_history = fopen(filepath,"a+");
@@ -75,51 +55,20 @@ int AddHistory(char *command,char *filepath)
 	return 0;
 }
 
-int add_history(char* command)
-{
-
-	char buffer[1024];
-	char *home = getenv("HOME");
-
-	if (home == NULL)
-	{
-		perror("Cannot Find the Home Directory");
-		return 1;
-	}
-	if (snprintf(buffer,1024,"%s/.history_C-Shell.txt",home) < 0)
-	{
-		return 1;
-	}
-		
-	FILE *fp = fopen(buffer,"a+");
-	
-	if (fp == NULL)
-	{
-		perror("Cannot Open The File For History.");
-		return 1;
-	}
-	
-	if (fprintf(fp,"%s\n",command)< 0)
-	{
-		perror("Error Writing To File.\n");
-		return 1;
-	}
-	
-	fclose(fp);
-	return 0;
-}
-
 char *GetHistory(int log)
 {
-	FILE *fp = fopen("/home/gigu/.history_C-Shell.txt","a+");
-	FILE *fp2 = fopen("/home/gigu/.history_C-Shell.txt","a+");
-	char *OutputBuffer = History(fp,fp2,4);
-	return OutputBuffer; // must free the buffer;
+	FILE *fp = fopen("h.txt","a+");
+	if (fp == NULL)
+	{
+		perror(" ");
+		return NULL;
+	}
+	
+	char *OutputBuffer = History(fp,4);
+	printf("%s\n",OutputBuffer); // must free the buffer;
 }
 
 int main()
 {
-	AddHistory("ls","h.txt");
-	AddHistory("cd","h.txt");
-	AddHistory("clear","h.txt");
+	GetHistory(1);
 }
